8ca25f0f5f9ba639c3cc522cc206e8dffc4c2aca
jagsanchez <jagsanchez@jagsanchezw7p.indra.es>
2019-01-25 12:29:38 +0100

    MESS variable set to true

--- emulator/src/consoleflex/main.java
+++ emulator/src/consoleflex/main.java
@@ -4,7 +4,7 @@ package consoleflex;
 import old.arcadeflex.osdepend;
 import static old2.arcadeflex.libc_v2.*;
 
-
+import static mame.ui_textH.MESS;
 /**
  *
  * @author shadow
@@ -15,6 +15,9 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // selects MESS
+        MESS = true;
+        
         ConvertArguments("consoleflex", args);
         args = null;
         System.exit(osdepend.main(argc, argv));
